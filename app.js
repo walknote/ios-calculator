8215bc5b6d07b60e77a8dd9f247a986bd5a2b005
walknote <112945084+walknote@users.noreply.github.com>
2022-12-30 20:50:10 +0530

    added few javascript variables

--- app.js
+++ app.js
@@ -2,7 +2,34 @@
 const hourEl = document.querySelector('.hour');
 const minuteEl = document.querySelector('.minute');
 const displayEl = document.querySelector('.display')
-const acEl = document.querySelector
+const acEl = document.querySelector('.ac')
+const pmEl = document.querySelector('.pm')
+const percentEl = document.querySelector('.percent')
+
+const additionEl = document.querySelector('.addition')
+const substractionEl = document.querySelector('.substraction')
+const multiplicationEl = document.querySelector('.multiplication')
+const divisionEl = document.querySelector('.divisioin')
+const equalEl = document.querySelector('.equal')
+
+const decimalEl = document.querySelector('.decimal')
+const number1El = document.querySelector('.number1')
+const number2El = document.querySelector('.number2')
+const number3El = document.querySelector('.number3')
+const number4El = document.querySelector('.number4')
+const number5El = document.querySelector('.number5')
+const number6El = document.querySelector('.number6')
+const number7El = document.querySelector('.number7')
+const number8El = document.querySelector('.number8')
+const number9El = document.querySelector('.number9')
+const number0El = document.querySelector('.number0')
+
+const numberElArray = [
+    number0El, number1El, number2El, number3El, number4El,
+    number5El, number6El, number7El, number8El, number9El
+];
+
+
 
 
 
